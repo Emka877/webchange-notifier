73fa1a93845c0781d600f68650f2cb800f94bc4f
Oscuro87 <capelle.e@gmail.com>
2021-12-03 13:17:23 +0100

    Rename misleading function name

--- src/config.rs
+++ src/config.rs
@@ -11,7 +11,7 @@ pub enum FileType {
 pub const BASE_FILENAME: &'static str = "base.txt";
 pub const COMPARISON_FILENAME: &'static str = "comparison.txt";
 
-pub fn get_relative_pathbuf_to(app_config: &AppConfig, file_type: FileType) -> PathBuf {
+pub fn get_absolute_pathbuf_to(app_config: &AppConfig, file_type: FileType) -> PathBuf {
     let working_dir: PathBuf = std::env::current_dir().unwrap();
     let path: String = match file_type {
         FileType::WriteType(WriteType::Base) => format!("{}/{}", app_config.relative_store_path.clone(), BASE_FILENAME),
